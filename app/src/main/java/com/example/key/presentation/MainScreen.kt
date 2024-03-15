10907a3cac22d34753ea3b55c9e73e2bc6a1a62d
Lobiofrom <124072945+Lobiofrom@users.noreply.github.com>
2024-03-15 17:44:53 +0300

    n

--- app/src/main/java/com/example/key/presentation/MainScreen.kt
+++ app/src/main/java/com/example/key/presentation/MainScreen.kt
@@ -18,18 +18,30 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,6 +53,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.key.R
 import com.example.key.viewmodel.DbViewModel
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
@@ -51,6 +64,9 @@ fun MainScreen(
     dbViewModel: DbViewModel = koinViewModel()
 ) {
     val context = LocalContext.current
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
 
     val menuItems by remember {
         mutableStateOf(
@@ -62,85 +78,147 @@ fun MainScreen(
     }
 
     val list = dbViewModel.list.collectAsState()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Box(
-        modifier = Modifier.fillMaxSize()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(stringResource(R.string.menu), modifier = Modifier.padding(16.dp))
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    label = { Text(text = stringResource(R.string.change_password)) },
+                    selected = true,
+                    onClick = {
+                        showDialog = true
+                    },
+                    modifier = Modifier.padding(6.dp)
+                )
+            }
+        },
     ) {
-        Column(
+        Box(
             modifier = Modifier.fillMaxSize()
         ) {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.secondaryContainer
-                ),
-                modifier = Modifier
-                    .statusBarsPadding()
-                    .clip(RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp)),
-                title = {
-                    Box(
-                        modifier = modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = stringResource(R.string.site_list),
-                            style = MaterialTheme.typography.titleLarge,
-                            fontSize = 22.sp
-                        )
-                    }
-                }
-            )
-
-            LazyColumn(
-                modifier = Modifier
-                    .navigationBarsPadding()
-                    .padding(6.dp)
+            Column(
+                modifier = Modifier.fillMaxSize()
             ) {
-                items(list.value,
-                    key = {
-                        it.id
-                    }) { item ->
-                    Item(
-                        item = item,
-                        menuItems = menuItems,
-                        modifier = Modifier.animateItemPlacement(
-                            //tween(durationMillis = 650)
-                            spring(
-                                dampingRatio = Spring.DampingRatioMediumBouncy,
-                                stiffness = Spring.StiffnessLow
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer
+                    ),
+                    modifier = Modifier
+                        .statusBarsPadding()
+                        .clip(RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp)),
+                    title = {
+                        Box(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = stringResource(R.string.site_list),
+                                style = MaterialTheme.typography.titleLarge,
+                                fontSize = 22.sp,
+                                modifier = Modifier.align(Alignment.Center)
                             )
-                        )
-                    ) { menu ->
-                        when (menu) {
-                            context.getString(R.string.edit) -> {
-                                navController.navigate("editScreen/${item.id}")
-                            }
-
-                            context.getString(R.string.delete) -> {
-                                dbViewModel.delete(item)
+                            IconButton(
+                                onClick = {
+                                    scope.launch {
+                                        drawerState.apply {
+                                            if (isClosed) open() else close()
+                                        }
+                                    }
+                                },
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            ) {
+                                Icon(Icons.Filled.Menu, contentDescription = null)
                             }
                         }
                     }
+                )
+                if (showDialog) {
+                    AlertDialog(onDismissRequest = { showDialog = false },
+                        text = {
+                            Box(
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.sure),
+                                    fontSize = 16.sp,
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                        },
+                        confirmButton = {
+                            Button(onClick = {
+                                navController.navigate("SetPasswordScreen")
+                                showDialog = false
+                            }) {
+                                Text(text = stringResource(R.string.yes))
+                            }
+                        },
+                        dismissButton = {
+                            Button(onClick = {
+                                showDialog = false
+                            }) {
+                                Text(text = stringResource(R.string.no))
+                            }
+                        }
+                    )
                 }
-                item {
-                    Spacer(modifier = Modifier.height(80.dp))
+
+                LazyColumn(
+                    modifier = Modifier
+                        .navigationBarsPadding()
+                        .padding(6.dp)
+                ) {
+                    items(list.value,
+                        key = {
+                            it.id
+                        }) { item ->
+                        Item(
+                            item = item,
+                            menuItems = menuItems,
+                            modifier = Modifier.animateItemPlacement(
+                                //tween(durationMillis = 650)
+                                spring(
+                                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                                    stiffness = Spring.StiffnessLow
+                                )
+                            )
+                        ) { menu ->
+                            when (menu) {
+                                context.getString(R.string.edit) -> {
+                                    navController.navigate("editScreen/${item.id}")
+                                }
+
+                                context.getString(R.string.delete) -> {
+                                    dbViewModel.delete(item)
+                                }
+                            }
+                        }
+                    }
+                    item {
+                        Spacer(modifier = Modifier.height(80.dp))
+                    }
                 }
             }
-        }
 
-        FloatingActionButton(
-            onClick = {
-                navController.navigate("addScreen")
-            },
-            modifier = modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp),
-            containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = Color.White
-        ) {
-            Icon(
-                Icons.Filled.Add, "Floating action button.",
-                modifier = Modifier.size(50.dp)
-            )
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate("addScreen")
+                },
+                modifier = modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp),
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            ) {
+                Icon(
+                    Icons.Filled.Add, "Floating action button.",
+                    modifier = Modifier.size(50.dp)
+                )
+            }
         }
     }
 }
